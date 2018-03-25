0be456366a44d1ec7876783741f537a2dfb439c7
Adam Dolaher <adam@Adams-MacBook-Air.local>
2018-03-25 17:05:43 -0400

    setup initial site paths

---
+++ distraction-blocker.py
@@ -0,0 +1,3 @@
+hosts_path="/etc/hosts"
+redirect="127.0.0.1"
+sitelist=["www.reddit.com","reddit.com","www.facebook.com","facebook.com","www.twitter.com","twitter.com","www.instagram.com","instagram.com","www.youtube.com","youtube.com","www.twitch.tv","twitch.tv"]
