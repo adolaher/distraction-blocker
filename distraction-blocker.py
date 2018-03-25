9815f400134d100a125c3167cc87bbcb8847cd31
Adam Dolaher <adam@Adams-MacBook-Air.local>
2018-03-25 17:21:26 -0400

    Setup initial script to edit hostfile based on sitelist.

--- distraction-blocker.py
+++ distraction-blocker.py
@@ -1,3 +1,18 @@
+import time
 hosts_path="/etc/hosts"
 redirect="127.0.0.1"
 sitelist=["www.reddit.com","reddit.com","www.facebook.com","facebook.com","www.twitter.com","twitter.com","www.instagram.com","instagram.com","www.youtube.com","youtube.com","www.twitch.tv","twitch.tv"]
+
+while True:
+    if dt(dt.now().year, dt.now().month,dt.now().day,9) < dt.now() < dt(dt.now().year, dt.now().month,dt.now().day,16)
+        print("You should be working")
+        with open(host_temp,'r+') as file:
+            content=file.read()
+            for site in sitelist:
+                if site in content:
+                    pass
+            else:
+                file.write(redirect+" "+site+"\n")
+    else:
+        print("Have fun!")
+    time.sleep(5)
