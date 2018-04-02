c19ade6ccc8025169a7d1acdc12a030a2eeccaff
Adam Dolaher <adam@Adams-MacBook-Air.local>
2018-04-01 21:18:21 -0400

    finishing touches

--- distraction-blocker.py
+++ distraction-blocker.py
@@ -1,7 +1,6 @@
 import time
 from datetime import datetime as dt
 
-hosts_temp="hosts"
 hosts_path="/etc/hosts"
 redirect="127.0.0.1"
 sitelist=["www.reddit.com","reddit.com","www.facebook.com","facebook.com","www.twitter.com","twitter.com","www.instagram.com","instagram.com","www.youtube.com","youtube.com","www.twitch.tv","twitch.tv"]
@@ -10,7 +9,7 @@ while True:
     if dt(dt.now().year, dt.now().month,dt.now().day,9) < dt.now() <
     dt(dt.now().year, dt.now().month,dt.now().day,16)
         print("You should be working")
-        with open(host_temp,'r+') as file:
+        with open(hosts_path,'r+') as file:
             content=file.read()
             for site in sitelist:
                 if site in content:
@@ -18,7 +17,7 @@ while True:
                 else:
                     file.write(redirect+" "+site+"\n")
     else:
-        with open(hosts_temp,'r+') as file:
+        with open(hosts_path,'r+') as file:
             content=file.readlines()
             file.seek(0)
             for line in content:
